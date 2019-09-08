00442e73d3432867238517311125cefccea33bfd
yhuihu <35516186+yhuihu@users.noreply.github.com>
2019-09-08 17:09:39 +0800

    增加数据库，代码漏传

--- spring-security-oauth2-resource/src/main/java/com/demo/oauth2/config/ResourceServerConfiguration.java
+++ spring-security-oauth2-resource/src/main/java/com/demo/oauth2/config/ResourceServerConfiguration.java
@@ -3,6 +3,7 @@ package com.demo.oauth2.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
@@ -12,6 +13,16 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.exceptionHandling()
+                .and()
+                //管理session的创建策略永远不会创建HttpSession，它不会使用HttpSession来获取SecurityContext
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                //ALWAYS总是创建HttpSession
+                //IF_REQUIREDSpring Security只会在需要时创建一个HttpSession
+                //NEVERSpring Security不会创建HttpSession，但如果它已经存在，将可以使用HttpSession
+                .and()
+                .authorizeRequests()
+                // 以下为配置所需保护的资源路径及权限，需要与认证服务器配置的授权部分对应
+                .antMatchers("/**").hasAuthority("SystemContent1");
     }
 }
